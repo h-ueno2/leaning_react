0b0b530c71ecdcd59187893f779d427ec9fcc59a
h-ueno2 <menbou1212@gmail.com>
2020-11-19 21:29:43 +0900

    TypeScriptでReact Hook。step2まで

--- ts-hook-tutorial/src/App.tsx
+++ ts-hook-tutorial/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import './App.css';
-import { BookToRead } from './BookToRead';
+import React, { useState } from "react";
+import "./App.css";
+import { BookToRead } from "./BookToRead";
+import BookRow from "./BookRow";
 
 const dummyBooks: BookToRead[] = [
   {
@@ -24,6 +25,31 @@ const dummyBooks: BookToRead[] = [
 ];
 
 const App = () => {
+  const [books, setBooks] = useState(dummyBooks);
+
+  const handleBookDelete = (id: number) => {
+    const newBooks = books.filter((b) => b.id !== id);
+    setBooks(newBooks);
+  }
+
+  const handleBookMemoChange = (id: number, memo: string) => {
+    const newBooks = books.map((b) => {
+      return b.id === id ? { ...b, memo: memo } : b;
+    });
+    setBooks(newBooks);
+  }
+
+  const bookRows = books.map((b) => {
+    return (
+      <BookRow
+        book={b}
+        key={b.id}
+        onMemoChange={(id, memo) => handleBookMemoChange(id, memo)}
+        onDelete={(id) => handleBookDelete(id)}
+      />
+    )
+  });
+
   return (
     <div className='App'>
       <section className='nav'>
@@ -31,7 +57,7 @@ const App = () => {
         <div className='button-like'>本を追加</div>
       </section>
       <section className='main'>
-        <h1>チュートリアルを始めましょう</h1>
+        { bookRows }
       </section>
     </div>
   );
