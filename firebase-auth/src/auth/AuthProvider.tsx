44b7332c434a0e1071c0e286e85a17c1e51a1329
h-ueno2 <menbou1212@gmail.com>
2020-12-19 00:01:30 +0900

    AuthContextの作成

---
+++ firebase-auth/src/auth/AuthProvider.tsx
@@ -0,0 +1,52 @@
+import React, { ReactChild, useEffect, useState } from "react";
+import { app } from "../base";
+
+// Contextを作成
+export const AuthContext = React.createContext<AuthContextProps | null>(null);
+
+// ContextのPropに使用する型
+export type AuthContextProps = {
+  login: (email: string, password: string, history: string[]) => void;
+  signup: (email: string, password: string, history: string[]) => void;
+  currentUser: firebase.default.User | null;
+};
+
+export const AuthProvider = (children: ReactChild) => {
+  const [currentUser, setCurrentUser] = useState<firebase.default.User | null>(
+    null
+  );
+
+  const login = async (email: string, password: string, history: string[]) => {
+    try {
+      await app.auth().signInWithEmailAndPassword(email, password);
+      history.push("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const signup = async (email: string, password: string, history: string[]) => {
+    try {
+      await app.auth().createUserWithEmailAndPassword(email, password);
+      history.push("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged(setCurrentUser);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        login: login,
+        signup: signup,
+        currentUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
