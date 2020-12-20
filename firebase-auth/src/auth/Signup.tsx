0eeaf958960fca5dbf0fb58a5dfb0bd7b92f9b60
h-ueno2 <menbou1212@gmail.com>
2020-12-20 12:06:15 +0900

    signupコンポーネント作製

--- firebase-auth/src/auth/Signup.tsx
+++ firebase-auth/src/auth/Signup.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { withRouter } from "react-router";
+import { AuthContext } from "./AuthProvider";
+import * as H from "history";
 
-const Signup = () => {
-  return <div></div>;
+type signupProps = {
+  history: H.History;
 };
 
-export default Signup;
+const Signup = (props: signupProps) => {
+  const { signup } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    signup(email, password, props.history);
+  };
+
+  return (
+    <div>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            onChange={handleEmailChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(Signup);
