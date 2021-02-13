843395e3d1a4ebf4e1c9fb5455a53e4a15a8a83b
h-ueno2 <menbou1212@gmail.com>
2021-02-13 22:16:11 +0900

    add: Row.tsx

--- clone-net-flix/src/App.tsx
+++ clone-net-flix/src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Row } from "./components/Row";
+import { requests } from "./request";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
+    </div>
+  );
 }
 
 export default App;
