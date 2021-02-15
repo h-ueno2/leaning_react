4a8dc7e8d93d2d1f507425d04cf0661cd9f26dc7
h-ueno2 <menbou1212@gmail.com>
2021-02-15 21:01:31 +0900

    update: Row.tsxにトレイラー表示を追加

--- clone-net-flix/src/components/Row.tsx
+++ clone-net-flix/src/components/Row.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import axios from "../axios";
 import "./Row.scss";
 
@@ -19,8 +20,17 @@ type Movie = {
   backdrop_path: string;
 };
 
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
+};
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTraierUrl] = useState<string | null>("");
 
   useEffect(() => {
     async function fetchData() {
@@ -30,6 +40,23 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts: Options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTraierUrl("");
+    } else {
+      let trailerurl = await axios.get(`/movie/${movie.id}/videos?api_key=~~~`);
+      setTraierUrl(trailerurl.data.results[0]?.key);
+    }
+  };
 
   console.log(movies);
 
@@ -46,10 +73,12 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
                 isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           );
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
